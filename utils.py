ac51bfe1a2efc86634faf3825154a0c62ebe3728
mahdi <mohammadmahdiabdollahpoor@gmail.com>
2019-01-21 01:02:14 +0330

    snow added

--- utils.py
+++ utils.py
@@ -1,10 +1,14 @@
 import numpy as np
+import random
 
 
 class Drop:
-    def __init__(self, x, y):
+    def __init__(self, x, y, min_speed, max_speed,type):
         self.x = x
         self.y = y
+        self.speed = random.randint( min_speed, max_speed)
+        self.type = type
+        self.sf = 0
 
 
 def write_image(small, large, upleftx, uplefty):
@@ -15,6 +19,17 @@ def write_image(small, large, upleftx, uplefty):
             rgb = small[i - upleftx, j - uplefty, :]
             inten = np.sum([x * x for x in rgb])
             if (inten > 15):
+                newimg[i, j, :] = small[i - upleftx, j - uplefty, :]
+    return newimg
+
+def write_image2(small, large, upleftx, uplefty):
+    newimg = large.copy()
+    sd = np.shape(small)
+    for i in range(upleftx, upleftx + sd[0]):
+        for j in range(uplefty, uplefty + sd[1]):
+            rgb = small[i - upleftx, j - uplefty, :]
+            inten = np.sum([x * x for x in rgb])
+            if (inten > 40):
                 newimg[i, j, :] = small[i - upleftx, j - uplefty, :]
     return newimg
 
--- webcammer.py
+++ webcammer.py
@@ -3,6 +3,7 @@ import cv2 as cv
 import random
 from utils import Drop
 from utils import *
+from time import time
 
 # cap = cv.VideoCapture('files\sample.mp4')
 cap = cv.VideoCapture(0)
@@ -13,48 +14,82 @@ fgbg = cv.createBackgroundSubtractorMOG2()
 
 kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3, 3))
 
+
 def separate(img):
     return fgbg.apply(img)
 
+
 drop_scale = 0.025
 s_img = cv.imread('files\drop.png')
+s2_img = cv.imread('files\snow.png')
 s_img = cv.resize(s_img, (0, 0), fx=drop_scale, fy=drop_scale)
+s2_img = cv.resize(s2_img, (0, 0), fx=drop_scale * 3, fy=drop_scale * 3)
+global added_image
+
+
+def overlay_drop(d):
+    global added_image
+    if (d.type == 0):
+        added_image = write_image(s_img, added_image, d.x, d.y)
+    else:
+        added_image = write_image2(s2_img, added_image, d.x, d.y)
+    return added_image
+
+
 print(np.shape(s_img))
-
-
+print(np.shape(s2_img))
 
 cnt = 0
 ssize = np.shape(s_img)
-max_drop_num = 50
+s2size = np.shape(s2_img)
+max_drop_num = 70
 drop_num = 0
-drop_speed = 10
+max_drop_speed = 10
+min_drop_speed = 5
 drops = []
-
+melt_time = 0
+# pool = Pool(processes=max_drop_num)
 while (True):
+    global added_image
     ret, l_img = cap.read()
     # print(np.shape(l_img))
     # l_img = cv.resize(l_img, (0, 0), fx=0.5, fy=0.5)
-
+    t = time()
     joda = separate(l_img)
     # print(np.shape(l_img))
     lsize = np.shape(l_img)
     cnt += 1
     for dr in drops:
         if (joda[dr.x + int(ssize[0] / 2), dr.y + int(ssize[1] / 2)] < 50):
-            dr.x += drop_speed
-        if (dr.x + ssize[0] >= lsize[0]):
-            drops.remove(dr)
+            dr.x += dr.speed
+        else:
+            dr.sf += 1
+            if(dr.sf > melt_time):
+                dr.type = 0
+        if (dr.type == 0):
+            if (dr.x + ssize[0] >= lsize[0]):
+                drops.remove(dr)
+        else:
+            if (dr.x + s2size[0] >= lsize[0]):
+                drops.remove(dr)
     added_image = l_img.copy()
-    for i in range(drop_num, max_drop_num):
-        d = Drop(0, random.randint(0, lsize[1] - ssize[1]))
-        drops.append(d)
-    drop_num = len(drops)
-    for d in drops:
-        added_image = write_image(s_img, added_image, d.x, d.y)
+    for i in range(0, 20):
+        if (random.randint(0, 2) % 2 == 0 and False):
+            d = Drop(0, random.randint(0, lsize[1] - ssize[1]), min_drop_speed, max_drop_speed, 0)
+        else:
+            d = Drop(0, random.randint(0, lsize[1] - s2size[1]), min_drop_speed, max_drop_speed, 1)
 
+        drops.append(d)
+        if (len(drops) >= max_drop_num):
+            break
+    drop_num = len(drops)
+    threads = []
+    for d in drops:
+        overlay_drop(d)
+    print(time() - t)
     cv.imshow('image', added_image)
     cv.imshow('joda', joda)
-    cv.waitKey(50)
+    cv.waitKey(3)
 
 # ret, l_img = cap.read()
 # cap.release()
